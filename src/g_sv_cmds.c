7f5a472b9d6cf9ff73254b510f534049b797136f
Jordy Rymenants <jordy.rymenants@outlook.com>
2016-07-28 13:03:28 +0200

    Removed remote command functionality (pointless with admin mod + filtering "login" and "password" from chat).

--- src/g_sv_cmds.c
+++ src/g_sv_cmds.c
@@ -436,16 +436,18 @@ __cdecl void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *cha
 
         if(textptr[0] == '/' || textptr[0] == '$' || (textptr[0] == '!' && !g_disabledefcmdprefix->boolean)){	//Check for Command-Prefix
 	    textptr++;
-	    SV_ExecuteRemoteCmd(ent->s.number, textptr);
+	    //SV_ExecuteRemoteCmd(ent->s.number, textptr);
 	    //Scr_PlayerSay(ent, mode, textptr -1);
 	    return;
         }
 
+	/*
 	if(strstr(textptr, "login") || strstr(textptr, "password"))
 	{
 	    SV_ExecuteRemoteCmd(ent->s.number, textptr);
 	    return;
 	}
+	*/
 
 	switch ( mode )
 	{
