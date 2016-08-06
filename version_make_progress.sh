b8eaa5bb9d8a4ff1bf8278ceb9b72e1d6720d5ce
Jordy Rymenants <jordy.rymenants@outlook.com>
2016-08-06 02:59:39 +0200

    Stripped carriage from version updating

--- version_make_progress.sh
+++ version_make_progress.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# increments the real number held in version.h by 0.01
 
 read line < src/version.h
 tokens=($line)
-version=${tokens[2]}
-newversion=$(echo $version + 1 | bc)
+version=$(echo "${tokens[2]}" | tr -d $'\r')
+newversion=$(echo "$version + 1" | bc)
 echo "#define BUILD_NUMBER $newversion" > src/version.h
